346bc92d029a810c7aed259e4df60f1355a92462
guscarrillo <115580058+guscarrillo@users.noreply.github.com>
2023-04-14 23:36:05 -0600

    wokred on aboute

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,15 +6,30 @@ export function About(){
         <div className='container'>
             <div className='about-info'>
                 {/** maybe an image of self when I get it */}
-                <Typography>
+                <Typography
+                    sx={{
+
+                    }}
+                >
                     Gustavo Carrillo
                 </Typography>
-                <Typography>
+                <Typography
+                    sx={{
+
+                    }}
+                >
                     Info about self
                 </Typography>
             </div>
-            <div className='Cube'>
+            <div 
+                className='cube comp'
+            >
                 {/** Figure out how to switch cubes when screen switches sizes */}
+            </div>
+            <div 
+                className='cube app'
+            >
+
             </div>
         </div>
         <div className='about-footer' id='Contact'>
